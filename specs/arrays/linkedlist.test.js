bd71a6d6270574a8eac39ce77451cccb961e2b6a
Deb Johnson <friendswithdrj@gmail.com>
2022-04-06 15:56:53 -0500

    Complete linked list

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -1,21 +1,21 @@
 /*
   LinkedList
-  
+
   Name your class / constructor (something you can call new on) LinkedList
-  
+
   LinkedList is made by making nodes that have two properties, the value that's being stored and a pointer to
   the next node in the list. The LinkedList then keep track of the head and usually the tail (I would suggest
   keeping track of the tail because it makes pop really easy.) As you may have notice, the unit tests are the
   same as the ArrayList; the interface of the two are exactly the same and should make no difference to the
   consumer of the data structure.
-  
+
   length - integer  - How many elements in the list
   push   - function - accepts a value and adds to the end of the list
   pop    - function - removes the last value in the list and returns it
   get    - function - accepts an index and returns the value at that position
-  delete - function - accepts an index, removes value from list, collapses, 
+  delete - function - accepts an index, removes value from list, collapses,
                       and returns removed value
-                      
+
   I would suggest making a second class, a Node class. However that's up to you how you implement it. A Node
   has two properties, value and next.
 
@@ -48,13 +48,11 @@ class LinkedList {
     for (let i = 0; i < index; i++) {
       current = current.next;
     }
-
     return current;
   }
   get(index) {
     const node = this._find(index);
-    if (!node) return void 0;
-    return node.value;
+    return node ? node.value : null;
   }
   delete(index) {
     if (index === 0) {
@@ -63,12 +61,10 @@ class LinkedList {
         this.head = head.next;
       } else {
         this.head = null;
-        this.tail = null;
       }
       this.length--;
       return head.value;
     }
-
     const node = this._find(index - 1);
     const excise = node.next;
     if (!excise) return null;
